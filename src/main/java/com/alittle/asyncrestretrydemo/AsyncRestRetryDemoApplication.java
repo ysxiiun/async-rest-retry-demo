aa8fcf422c8146ca4da2d3230192bd2ed8dc5e39
youshixin <youshixin_i@didiglobal.com>
2019-11-27 11:36:22 +0800

    创建项目

---
+++ src/main/java/com/alittle/asyncrestretrydemo/AsyncRestRetryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alittle.asyncrestretrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncRestRetryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncRestRetryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alittle/asyncrestretrydemo/AsyncRestRetryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alittle.asyncrestretrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncRestRetryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
